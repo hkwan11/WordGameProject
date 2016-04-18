1deb886f7bc6d5a9e17c78a4530eed945d9152a1
Robert Bobby Cowen <rac040@bucknell.edu>
2016-04-18 18:29:25 -0400

    update with basic input structure

--- wordgame.py
+++ wordgame.py
@@ -5,3 +5,18 @@ def main():
     if len(sys.argv) != 1:
         print("ERROR - enter correct number of parameters")
         exit()
+    
+    fileName = sys.argv[1]
+    
+    try:
+        #try to read in file
+        file = open(fileName, "r")
+        
+    except IOError:
+        print("ERROR - Unable to open specified file " + fileName)
+        exit()
+    
+    lines = fileName.readlines()
+    
+    for line in lines:
+        
