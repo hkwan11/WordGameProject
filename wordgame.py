686cbf5b9f3ffcd545c1fbb4e9351423c86cb40b
Robert Bobby Cowen <rac040@bucknell.edu>
2016-04-20 11:53:10 -0400

    pretty much done, need to figure out command line run

--- wordgame.py
+++ wordgame.py
@@ -6,7 +6,7 @@ def main():
 ##        print("ERROR - Enter correct parameters - wordgame.py [word1] [word2]")
 ##        return
 ##    fileName = sys.argv[1]
-    fileName = "5lw-s.dat"
+    fileName = "5lw-m.dat"
     
     try:
         #try to read in file
@@ -21,33 +21,54 @@ def main():
     wordGraph = createGraph(lines)
 
     #Run test based on user input
-    runTrial(wordGraph)
+    
+    rerun = runTrial(wordGraph)
+    while(rerun):
+        rerun = runTrial(wordGraph)
 
 
 def createGraph(lines):
     wordGraph = {}
     
     for line in lines:
-        words = line.split(" ")
+        l = line.split("\n")
+        words = l[0].split(" ")
 
         for word in words:
-            v = Vertex(word)
-            A = [[-1 for x in range(5)] for x in range(5)]
-            neighborList = []
-            
-            for k in wordGraph:
-                lcsScore = 5 - LCS_len(word, k, A)
+            if len(word) > 0 and word != "\n":
+                v = Vertex(word)
+                neighborList = []
+                
+                for k in wordGraph:
+                    missScore = getMissScore(word, k, len(word))
 
-                if lcsScore >= 3:
-                    neighborList.append(k)
-                    wordGraph[k].append(v)
-            try:
-                wordGraph[word].append(neighborList)
-            except KeyError:
-                wordGraph[word] = neighborList
+                    if missScore != -1:
+                        vertexK = Vertex(k)
+                        neighborList.append(vertexK)
+                        wordGraph[k].append(v)
+                try:
+                    wordGraph[word].append(neighborList)
+                except KeyError:
+                    wordGraph[word] = neighborList
 
     return wordGraph
-            
+
+def getMissScore(w1, w2, wLen):
+    missScore = 0
+
+    for i in range(wLen):
+        #print("WORD IS: " + w1)
+        if w1[i] != w2[i]:
+            missScore = missScore + 1
+
+    if missScore == 2:
+        return 5
+    elif missScore == 1:
+        return 1
+    elif missScore == 0:
+        return 0
+    else:
+        return -1
 
 def runTrial(wordGraph):
     #asks the user to input word to check
@@ -58,43 +79,54 @@ def runTrial(wordGraph):
         userWord = userWord.upper()
 
         if len(userWord) != 5:
-            raise ValueError("Whoops! Didn't enter a five letter word")
+            raise ValueError("Whoops! Didn't enter a five letter word!")
+        else:
+            print()
+
+            #Test to see if user word in the neighbor list
+            try:
+                neighborList = wordGraph[userWord]
+
+                print("The neighbors of SHALE are:")
+                numTotal = 0
+
+                for v in neighborList:
+                    A = [[-1 for x in range(4)] for x in range(4)]
+                    #score = LCS_len(userWord, v.getWord(), A)
+                    score = getMissScore(userWord, v.getWord(), 5)
+
+                    if numTotal % 6 == 0:
+                        print()
+                    print(v.getWord() + " (" + str(score) + ") ", end = " ")
+                    numTotal = numTotal + 1
+                
+            except KeyError:
+                print(userWord + " is not in the graph!")
         
     #if incorrect length, it is caught
     except ValueError as err:
         print(err.args)
 
-    #Test to see if user word in the neighbor list
-##    try:
-##        neighborList = wordGraph[userWord]
-##
-##        for v in neighborList:
-##            A = [[-1 for x in range(4)] for x in range(4)]
-##            score = LCS_len(userWord, v.getKey(), A)
-##            print(v.getKey() + " (" + str(score) + ") ", end = " ")
-##        
-##    except KeyError:
-##        print(userWord + " is not in the graph!")
-
     ### ----Ask User If they desire to do another trial---- ###
-    while(True):
-        doTrial = input("Do you wish to complete another trial? (Y/Yes, N/No) ")
-        doTrial = doTrial.upper()
+    doTrial = input("\nDo you wish to complete another trial? (Y/Yes, N/No) ")
+    doTrial = doTrial.upper()
+    #print(doTrial)
 
-        if doTrial is "Y" or doTrial is "YES":
-            runTrial()
-        elif doTrial is "N" or doTrial is "NO":
-            break
-        else:
-            print("Sorry, that is not a valid input")
+    if doTrial == "Y" or doTrial == "YES":
+        return 1
+    elif doTrial == "N" or doTrial == "NO":
+        return 0
+    else:
+        print("Sorry, that is not a valid input")
 
 
 class Vertex:
     """Vertex Class"""
 
-    def __init__(self, key):
-        self.key = key
+    def __init__(self, word):
+        self.key = -1
         self.handle = -1
+        self.word = word
 
     def getHandle(self):
         return self.handle
@@ -104,4 +136,7 @@ class Vertex:
 
     def getKey(self):
         return self.key
+
+    def getWord(self):
+        return self.word
 
